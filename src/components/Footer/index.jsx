db70547ade598498ff6cd9030d4b593d15deca7f
Mohamed TCHOKETCH <mohamedtchoketch0@gmail.com>
2023-10-20 21:18:13 +0200

    feat(footer): create footer
    create responsive footer with the implementation of translation
    
    Closes #9

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,153 @@
+// import Button
+import Image from "next/image";
+import Link from "next/link";
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import React from "react";
+import {
+    FaFacebookF,
+    FaInstagram,
+    FaLinkedin,
+    FaTwitter,
+    FaYoutube,
+} from "react-icons/fa";
+
+const Footer = () => {
+    const current_year = new Date().getFullYear();
+    const { t } = useTranslation("footer");
+    return (
+        <div>
+            <div className='container mx-auto text-white bg-[#43AFD6] py-6'>
+                <div className='grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4'>
+                    <div className='space-y-3'>
+                        <div>
+                            <Image
+                                src='/footer_icons/logo.svg'
+                                alt='logo-image'
+                                width='40'
+                                height='70'
+                            />
+                        </div>
+                        <div>
+                            <h1 className='text-3xl'>
+                                <b>Food on coupon</b>
+                            </h1>
+                        </div>
+                        <div>
+                            <p>{t("footer.description")}</p>
+                        </div>
+                    </div>
+                    <div className='space-y-3'>
+                        <div className='space-y-1 space-x-10'>
+                            <Link className='text-white' href='/Home'>
+                                <b>{t("footer.homePage")}</b>
+                            </Link>
+                            <Link className='text-white' href='/Coupons'>
+                                <b>{t("footer.couponsPage")}</b>
+                            </Link>
+                            <Link className='text-white' href='/Blog'>
+                                <b>{t("footer.blogPage")}</b>
+                            </Link>
+                        </div>
+                        <div className='flex flex-col items-center space-y-4 md:flex-row md:space-y-0 md:space-x-4'>
+                            <div
+                                className='flex items-center flex-col space-x-2 text-center'
+                                style={{ flex: "1" }}
+                            >
+                                <Image
+                                    src='/footer_icons/map-marker.svg'
+                                    alt='map-marker'
+                                    width='40'
+                                    height='40'
+                                />
+                                <p>{t("footer.address")}</p>
+                            </div>
+                            <div
+                                className='flex items-center flex-col space-x-2 text-center'
+                                style={{ flex: "1" }}
+                            >
+                                <Image
+                                    src='/footer_icons/phone-marker.svg'
+                                    alt='phone-marker'
+                                    width='40'
+                                    height='40'
+                                />
+                                <p>
+                                    +229-955-5388-336 <br />
+                                    +229-955-5373-360
+                                </p>
+                            </div>
+                            <div
+                                className='flex items-center flex-col space-x-2 text-center'
+                                style={{ flex: "1" }}
+                            >
+                                <Image
+                                    src='/footer_icons/email-marker.svg'
+                                    alt='email-marker'
+                                    width='40'
+                                    height='40'
+                                />
+                                <p>
+                                    office@mail.com <br />
+                                    support@mail.com
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='space-y-3'>
+                        <div>
+                            <h3 className='pb-5'>
+                                <b>{t("footer.subNewsletter")}</b>
+                            </h3>
+                        </div>
+                        <div className='flex items-center'>
+                            <div>
+                                <input
+                                    type='email'
+                                    className='p-1'
+                                    placeholder={t("footer.emailEnter")}
+                                />
+                            </div>
+                            <div>
+                                <button className='submit-button bg-[#20DC49] p-1 pl-3 pr-3 text-white'>
+                                    {t("footer.submit")} →
+                                </button>
+                            </div>
+                        </div>
+                        <div className='flex items-center space-x-10'>
+                            <a href='http://www.twitter.com'>
+                                <FaTwitter className='text-2xl' />
+                            </a>
+                            <a href='http://www.linkedin.com'>
+                                <FaLinkedin className='text-2xl' />
+                            </a>
+                            <a href='http://www.instagram.com'>
+                                <FaInstagram className='text-2xl' />
+                            </a>
+                            <a href='http://www.facebook.com'>
+                                <FaFacebookF className='text-2xl' />
+                            </a>
+                            <a href='http://www.youtube.com'>
+                                <FaYoutube className='text-2xl' />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='bg-[#20DC49] h-10 flex justify-center items-center text-white'>
+                <p>
+                    © {current_year} {t("footer.right")}
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
+export async function getStaticProps({ locale }) {
+    return {
+        props: {
+            ...(await serverSideTranslations(locale, ["footer"])),
+        },
+    };
+}
