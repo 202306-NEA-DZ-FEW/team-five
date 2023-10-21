b9770a8b70b0458741124055c7cb9a5e7d6baab6
Sidahmed Slikh <sidahmedzinedine@gmail.com>
2023-10-21 17:43:48 +0200

    feat(footer navbar layout): layout nav and footer
    Implement the layout for the Navbar and Footer components in the project,
    
    Closes #10

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -4,7 +4,7 @@ import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import CouponUsed from "@/containers/Admin/CouponUsed";
 
 const Admin = () => {
-    const { t } = useTranslation("admin");
+    const { t } = useTranslation("admin", "footer", "common");
     return (
         <div className='grid grid-cols-1fr grid-rows-1fr grid-flow-row grid-areas-3 sm:grid-cols-1fr md:grid-cols-1fr lg:grid-cols-1fr xl:grid-cols-1/3'>
             <div className='UsedCoupons ml-2 md:ml-20 mr-2 md:mr-20'>
@@ -21,7 +21,11 @@ export default Admin;
 export async function getStaticProps({ locale }) {
     return {
         props: {
-            ...(await serverSideTranslations(locale, ["admin"])),
+            ...(await serverSideTranslations(locale, [
+                "admin",
+                "footer",
+                "common",
+            ])),
         },
     };
 }
