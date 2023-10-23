ee3ada8152ad5b4b8e0d4bb9041c486e111adfeb
Achraf Mehanneche <achrafmehenneche.am@gmail.com>
2023-10-23 13:40:30 +0100

    feat: creating BlogCard that have the have the changing blog title and image, author, and date

---
+++ src/components/BlogCard/index.jsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import { useTranslation } from "next-i18next";
+
+const BlogCard = ({ blog }) => {
+    const { t } = useTranslation("BlogCard");
+    return (
+        <div className='max-w-sm rounded overflow-hidden shadow-lg m-4'>
+            <img
+                src={blog.image}
+                alt={blog.title}
+                className='w-full h-56 object-cover'
+            />
+            <div className='p-4'>
+                <h2 className='font-bold text-xl mb-2'>{blog.title}</h2>
+                <div className='flex items-center mb-2'>
+                    <img
+                        src={blog.authorImage}
+                        alt={blog.author}
+                        className='w-8 h-8 rounded-full mr-2'
+                    />
+                    <span className='text-gray-700'>{blog.author}</span>
+                </div>
+                <p className='text-gray-600 mb-4'>{blog.shortDescription}</p>
+                <Link href={`/blog/${blog.slug}`}>
+                    <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full'>
+                        <p>{t("Read More")}</p>
+                    </button>
+                </Link>
+            </div>
+        </div>
+    );
+};
+
+export default BlogCard;
