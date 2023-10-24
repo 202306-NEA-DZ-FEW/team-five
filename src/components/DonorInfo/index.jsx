4122c69829817cbcf6b5c3bad37fc650b369f585
Mohamed TCHOKETCH <mohamedtchoketch0@gmail.com>
2023-10-24 22:33:55 +0200

    feat(donor information): create donor information section
    create the donor information section and the first and last name input section
    
    Closes #50

--- src/components/DonorInfo/index.jsx
+++ src/components/DonorInfo/index.jsx
@@ -1,12 +1,15 @@
+import { useTranslation } from "next-i18next";
 import React, { useState } from "react";
-// import { useTranslation } from "next-i18next";
 
 function DonorInformation() {
     const [donationType, setDonationType] = useState("personal");
-    // const { t } = useTranslation("")
+    const { t } = useTranslation("payment");
     return (
         <div className=' flex flex-col items-center'>
             <div className='flex justify-between w-96 mb-4'>
+                <h1 className='text-[#20DC49] text-left'>
+                    {t("payment.donoInfo")}
+                </h1>
                 <div className='personal flex items-center bg-[#20DC49]'>
                     <input
                         type='radio'
@@ -15,7 +18,7 @@ function DonorInformation() {
                         checked={donationType === "personal"}
                         onChange={() => setDonationType("personal")}
                     />
-                    Personal Donation
+                    {t("payment.persDonation")}
                     {donationType === "personal" && (
                         <span className='ml-2 text-white'>&#10003;</span>
                     )}
@@ -28,29 +31,32 @@ function DonorInformation() {
                         checked={donationType === "organization"}
                         onChange={() => setDonationType("organization")}
                     />
-                    Organization Donation
+                    {t("payment.orgDonation")}
                     {donationType === "organization" && (
                         <span className='ml-2 text-white'>&#10003;</span>
                     )}
                 </div>
             </div>
             <div className='flex justify-between w-96'>
+                <h2 className='text-[#20DC49] text-left'>
+                    {t("payment.name")}
+                </h2>
                 <div className='w-1/2'>
-                    <div className='first-name'>First Name:</div>
+                    <div className='first-name'>{t("payment.firstName")}</div>
                     <input
                         type='text'
                         id='first-name'
                         className='w-full'
-                        // placeholder={t("")}
+                        placeholder={t("payment.firstName")}
                     />
                 </div>
                 <div className='w-1/2'>
-                    <div className='last-name'>Last Name:</div>
+                    <div className='last-name'>{t("payment.lastName")}</div>
                     <input
                         type='text'
                         id='last-name'
                         className='w-full'
-                        // placeholder={t("")}
+                        placeholder={t("payment.lastName")}
                     />
                 </div>
             </div>
