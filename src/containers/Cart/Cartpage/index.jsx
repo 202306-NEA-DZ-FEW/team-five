b6ed12f511c4fdcaf0cb4388b8d92e440638faab
Sidahmed Slikh <sidahmedzinedine@gmail.com>
2023-10-28 01:02:51 +0200

    style(cartpage): style and Add Translation to Cart Page
    Enhance the Cart Page by improving its visual style and adding translation
    
    Closes #78

--- src/containers/Cart/Cartpage/index.jsx
+++ src/containers/Cart/Cartpage/index.jsx
@@ -1,29 +1,24 @@
-import FormatePrice from "@/pages/auth/FormatePrice";
+import { addDoc, collection } from "firebase/firestore";
+import { useTranslation } from "next-i18next";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import {
-    deleteItem,
-    minusQuantity,
-    plusQuantity,
-    resetCart,
-} from "@/redux/shopperSlice";
-import Link from "next/link";
-import { addDoc, collection, getDocs } from "firebase/firestore";
-import { auth, db } from "@/Utils/firebase";
-import Image from "next/image";
-import CouponCard from "@/components/Cards/CouponCard";
+
 import CheckoutCard from "@/components/Cards/CheckoutCard";
+import CouponCard from "@/components/Cards/CouponCard";
+
+import { resetCart } from "@/redux/shopperSlice";
+import { auth, db } from "@/Utils/firebase";
 
 function CartPage() {
+    const { t } = useTranslation("cart", "footer", "common");
     const productData = useSelector((state) => state.shopper.productData);
     const dispatch = useDispatch();
     const [totalOldPrice, setTotalOldPrice] = useState(0);
     const [totalSavings, setTotalSavings] = useState(0);
     const [totalamt, setTotalAmt] = useState(0);
-    const userInfo = useSelector((state) => state.shopper.userInfo);
 
     const cartCollectionRef = collection(db, "Cart");
-    const addcart = async (e) => {
+    const addcart = async () => {
         await addDoc(cartCollectionRef, {
             name: productData,
             uid: auth?.currentUser?.uid,
@@ -52,48 +47,42 @@ function CartPage() {
     return (
         <div className='max-w-contentContainer bg-gray-100'>
             <div className='w-full py-10'>
-                <div className='w-full flex '>
-                    <div className='w-2/3 flex flex-col gap-5'>
-                        <h1 className='text-2xl font-bold text-black'>
-                            Cart{" "}
-                            <span className='text-lightText font-normal'>
-                                ({productData.length} items)
+                <div className='w-full flex gap-5'>
+                    <div className='ml-10 w-2/3 flex flex-col gap-5 flex-wrap items-center'>
+                        <h1 className='text-2xl font-bold text-black gap-5 text-left items-start'>
+                            {t("cart.title")}{" "}
+                            <span className='text-lightText font-normal gap-5'>
+                                ({productData.length} {t("cart.item")} )
                             </span>
                         </h1>
                         <div>
-                            <div className='text-xl  font-bold flex flex-col gap-2 mb-2 relative bg-white border rounded-[20px]  p-4 shadow-md md:w-[500px] lg:w-[600px] xl:w-[950px]'>
-                                <p>All Coupons :</p>
-
+                            <div className='text-xl gap-5 font-bold flex flex-col mb-2 relative bg-white border rounded-2xl p-4 shadow-md md:w-[500px] lg:w-[700px] xl:w-[900px] mx-auto'>
+                                <p>{t("cart.items")}</p>
                                 <div>
                                     {productData.map((item) => (
-                                        <>
-                                            <div
-                                                key={item._id}
-                                                className='flex items-center justify-center  border-b-[1px] border-b-zinc-200 pb-4'
-                                            >
-                                                <CouponCard
-                                                    quantity={item.quantity}
-                                                    image={item.image}
-                                                    UserName={item.UserName}
-                                                    price={item.price}
-                                                    _id={item._id}
-                                                    ProductImg={item.ProductImg}
-                                                    CouponAmount={
-                                                        item.CouponAmount
-                                                    }
-                                                />
-                                            </div>
-                                        </>
+                                        <div
+                                            key={item._id}
+                                            className='flex items-center justify-center border-b border-zinc-200 pb-4'
+                                        >
+                                            <CouponCard
+                                                quantity={item.quantity}
+                                                image={item.image}
+                                                UserName={item.UserName}
+                                                price={item.price}
+                                                _id={item._id}
+                                                ProductImg={item.ProductImg}
+                                                CouponAmount={item.CouponAmount}
+                                            />
+                                        </div>
                                     ))}
                                 </div>
-
                                 <button onClick={() => dispatch(resetCart())}>
-                                    Reset Cart
+                                    {t("cart.reset")}
                                 </button>
                             </div>
                         </div>
                     </div>
-                    <div className=' mt-24 h-[500px]  flex flex-col justify-center'>
+                    <div className='mt-24 h-500 flex flex-col justify-center'>
                         <CheckoutCard
                             totalOldPrice={totalOldPrice}
                             totalSavings={totalSavings}
--- src/pages/cart/index.js
+++ src/pages/cart/index.js
@@ -1,12 +1,29 @@
-import CartPage from "@/containers/Cart/Cartpage";
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import React from "react";
 
+import CartPage from "@/containers/Cart/Cartpage";
+
 const Cart = () => {
+    const { t } = useTranslation("cart", "footer", "common");
+
     return (
-        <div>
+        <div Classname='overflow-x-hidden'>
             <CartPage />
         </div>
     );
 };
 
 export default Cart;
+
+export async function getStaticProps({ locale }) {
+    return {
+        props: {
+            ...(await serverSideTranslations(locale, [
+                "cart",
+                "footer",
+                "common",
+            ])),
+        },
+    };
+}
