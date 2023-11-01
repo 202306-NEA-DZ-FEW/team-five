4e4a698f5fca00635eb941eeeb817fb6033caf19
Sidahmed Slikh <sidahmedzinedine@gmail.com>
2023-11-01 01:11:46 +0100

    fix(disc): fix vercel problem
    Closes #98

--- src/components/Cards/Disc.jsx
+++ src/components/Cards/Disc.jsx
@@ -2,7 +2,6 @@ import { useTranslation } from "next-i18next";
 import React from "react";
 import Email from "@/components/Email/Email";
 import BlogCard from "@/components/BlogCard";
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 const Disc = () => {
     const { t } = useTranslation("cart", "footer", "common");
@@ -11,15 +10,3 @@ const Disc = () => {
 };
 
 export default Disc;
-
-export async function getStaticProps({ locale }) {
-    return {
-        props: {
-            ...(await serverSideTranslations(locale, [
-                "cart",
-                "footer",
-                "common",
-            ])),
-        },
-    };
-}
