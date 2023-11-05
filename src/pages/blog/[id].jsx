9aa7c09ea3119152ec324e5906fda1b5774dd1a4
Sidahmed Slikh <sidahmedzinedine@gmail.com>
2023-11-05 19:09:18 +0100

    fix(article page): vercel problem
    Closes #112

--- src/pages/blog/[id].jsx
+++ src/pages/blog/[id].jsx
@@ -1,7 +1,6 @@
 import { useTranslation } from "next-i18next";
 import React from "react";
 import Article from "@/components/Cards/Article";
-import { getTranslations } from "@/Utils/translation";
 
 function BlogSingle({ article }) {
     const { t } = useTranslation([
@@ -21,52 +20,10 @@ function BlogSingle({ article }) {
 
 export default BlogSingle;
 
-export async function getStaticPaths({ locales }) {
-    const paths = [];
-
-    for (const locale of locales) {
-        const apiToken = process.env.NEXT_PUBLIC_API_TOKEN;
-        let endpoint = "";
-
-        if (locale === "ar") {
-            endpoint = `https://gnews.io/api/v4/search?q=food&token=${apiToken}&lang=ar`;
-        } else {
-            endpoint = `https://gnews.io/api/v4/search?q=food%20problems%20hunger&token=${apiToken}&lang=en`;
-        }
-
-        const response = await fetch(endpoint);
-
-        if (response.ok) {
-            const data = await response.json();
-            const articles = data.articles;
-
-            for (const article of articles) {
-                paths.push({
-                    params: { id: encodeURIComponent(article.title) },
-                    locale,
-                });
-            }
-        }
-    }
-
-    return {
-        paths,
-        fallback: false,
-    };
-}
-
-export async function getStaticProps({ locale, params }) {
-    const id = params.id;
-    const translations = await getTranslations(locale);
-
+export async function getServerSideProps({ params, locale }) {
     const apiToken = process.env.NEXT_PUBLIC_API_TOKEN;
-    let endpoint = "";
 
-    if (locale === "ar") {
-        endpoint = `https://gnews.io/api/v4/search?q=food&token=${apiToken}&lang=ar`;
-    } else {
-        endpoint = `https://gnews.io/api/v4/search?q=food%20problems%20hunger&token=${apiToken}&lang=en`;
-    }
+    const endpoint = `https://gnews.io/api/v4/search?q=food%20problems%20hunger&token=${apiToken}&lang=en`;
 
     const response = await fetch(endpoint);
 
@@ -74,19 +31,14 @@ export async function getStaticProps({ locale, params }) {
         const data = await response.json();
         const articles = data.articles;
         const foundArticle = articles.find(
-            (a) => a.title === decodeURIComponent(id)
+            (a) => a.title === decodeURIComponent(params.id)
         );
-
         return {
-            props: {
-                translations,
-                article: foundArticle,
-                articles: articles,
-            },
-        };
-    } else {
-        return {
-            notFound: true,
+            props: { article: foundArticle, articles },
         };
     }
+
+    return {
+        props: {},
+    };
 }
