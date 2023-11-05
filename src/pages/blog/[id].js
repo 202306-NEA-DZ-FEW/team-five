31a90b0db3ea09a8887ebaa7b257493645743525
Sidahmed Slikh <sidahmedzinedine@gmail.com>
2023-11-05 03:57:21 +0100

    fix(article page): vercel problem
    Closes #112

--- src/pages/blog/[id].js
+++ src/pages/blog/[id].js
@@ -1,6 +1,7 @@
 import { useTranslation } from "next-i18next";
 import React from "react";
 import Article from "@/components/Cards/Article";
+import { getTranslations } from "@/Utils/translation";
 
 function BlogSingle() {
     const { t } = useTranslation(
@@ -19,3 +20,22 @@ function BlogSingle() {
 }
 
 export default BlogSingle;
+
+export const getStaticProps = async ({ locale }) => {
+    const translations = await getTranslations(locale);
+
+    return {
+        props: {
+            ...translations,
+        },
+    };
+};
+
+export async function getStaticPaths() {
+    const paths = [];
+
+    return {
+        paths,
+        fallback: false,
+    };
+}
