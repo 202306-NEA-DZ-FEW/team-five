1d980fb490925ec1fa3b727f952e7763211dd6d5
Sidahmed Slikh <sidahmedzinedine@gmail.com>
2023-11-05 21:53:54 +0100

    fix(articlepage): vercel pproblem
    Closes #113

--- src/pages/blog/[id].jsx
+++ src/pages/blog/[id].jsx
@@ -2,22 +2,38 @@ import { useTranslation } from "next-i18next";
 import React from "react";
 import Article from "@/components/Cards/Article";
 import "core-js/modules/es.array.map";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
-function BlogSingle() {
+function BlogSingle({ article }) {
     const { t } = useTranslation([
         "cart",
         "footer",
         "emails",
         "navbar",
         "common",
-        "blog",
     ]);
 
     return (
         <div className='text-center'>
-            <Article />
+            <Article article={article} />
         </div>
     );
 }
 
 export default BlogSingle;
+
+export async function getServerSideProps({ locale }) {
+    return {
+        props: {
+            ...(await serverSideTranslations(locale, [
+                "cart",
+                "footer",
+                "emails",
+                "navbar",
+                "common",
+                "blog",
+            ])),
+            // Will be passed to the page component as props
+        },
+    };
+}
