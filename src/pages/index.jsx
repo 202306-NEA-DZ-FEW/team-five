87b7c02bb19b578c9f439ffa372dc8634a4fd8ca
Sidahmed Slikh <sidahmedzinedine@gmail.com>
2023-11-08 21:30:54 +0100

    style(coupons page): fix CouponPage Design and Add Animations
    Enhance the project by addressing the following tasks: fixing the coupon design to make it more
    visually appealing and adding animations to create a more engaging user experience
    
    Closes #132

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,45 +2,14 @@ import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import React from "react";
 
-import Banner from "@/components/Banner";
-import Goal from "@/components/donation/Goal";
-import Steps from "@/components/donation/Steps";
-import Email from "@/components/Email/Email";
-import MemberList from "@/components/Members/MemberList";
-import Sponsor from "@/components/sponsors/sponsor";
-import Blogcontainer from "@/containers/Blogcontainer";
-import Newsl from "@/containers/Newsl";
+import Home from "@/containers/Home";
 
 function Homepage() {
     const { t } = useTranslation("common", "blog", "footer", "navbar");
 
     return (
         <>
-            <Newsl />
-            <Banner
-                btnText={t("btn")}
-                imageUrl='/BannerHome.svg'
-                description={t("desc")}
-                title={t("title")}
-                title1={t("title1")}
-                title2={t("title2")}
-            />
-            <Steps />
-            <div className='mb-14 '>
-                <Goal />
-            </div>
-            <div className='bg-[#F9FAFB]  '>
-                <Blogcontainer />
-                <div className='flex justify-center'>
-                    <Email />
-                </div>
-            </div>
-            <div className='flex justify-center '>
-                <MemberList />
-            </div>
-            <div className='bg-[#F9FAFB] '>
-                <Sponsor />
-            </div>
+            <Home />
         </>
     );
 }
